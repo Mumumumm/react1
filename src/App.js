69e4d09b50a38a230992e08d2304a3d8baa9ca2e
김준홍 <rlawnsghd434343@gmail.com>
2025-03-26 23:19:49 +0900

    03-26 1_컴포넌트생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+//-------------------Header 컴포넌트------------------------
+function Header() {
+  return (
+    <header>
+      <h1><a href='/'>Header component</a></h1>
+    </header>
+  );
+};
+
+//-------------------Nav 컴포넌트-----------------------
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href='/read/1'>HTML</a></li>
+        <li><a href='/read/2'>CSS</a></li>
+        <li><a href='/read/3'>JavaScript</a></li>
+      </ol>
+    </nav>
+  );
+};
+
+//----------------Article 컴포넌트---------------------------
+function Article() {
+  return (
+    <article>
+      <h2>Wlecome</h2>
+      Hello, Web
+    </article>
+  );
+};
+
+
+//-----------------부모App--------------------------
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
